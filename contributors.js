52c31f55de212264b6d2b250c670fb99fa9cf851
Remy D <remyd@civx.us>
2011-05-02 23:54:56 -0400

    Added a bunch of Meetings, but not all of them yet. atleast I now have a somewhat of an inventory of all the meetings ready to be jsonified...

--- contributors.js
+++ contributors.js
@@ -2,10 +2,19 @@
     types: {
         "Contributor" : {
             pluralLabel: "Contributors",
+        "Team Meeting" : {
+            pluralLabel: "Team Meetings",
+        "Dev Meeting" : {
+            pluralLabel: "Dev Meetings",
+        "Design Meeting" : {
+            pluralLabel: "Design Meetings",
+        "Test Event" : {
+            pluralLabel: "Test Events",
         "Event" : {
-            pluralLabel: "Events",
-        "Meeting" : {
-            pluralLabel: "Meetings"
+            pluralLabel: "Events"
+        }
+        }
+        }
         }
         }
         }
@@ -69,20 +78,116 @@
             url :                   "http://opl.rit.edu",
             imageURL :              "http://nobelprize.org/nobel_prizes/chemistry/laureates/1990/corey_thumb.jpg"
         },
-        {   type :                  "Meeting",
-            label :                 "Rise Meeting #1",
+        {   type :                  "Team Meeting",
+            label :                 "Meet the Developers",
             discipline :            "Team Meeting",
             date :                  "2010-12-17",
-            relationship :          "Meeting",
-            "relationship-detail" : "First Meeting of Rise above the Crowd",
+            relationship :          "Team Meeting",
+            "relationship-detail" : "First Meeting of Rise Above The Crowd Team with Developers",
             url :                   "knightmeeting121710.txt"
         },
         {   type :                  "Event",
+            label :                 "Scoping Meeting",
+            discipline :            "Advisory Meeting",
+            date :                  "2010-12-10",
+            relationship :          "Team Meeting",
+            "relationship-detail" : "Talked through broad-view and scope of project, and how to start a design/dev team",
+            url :                   "knightfriday121010.txt"
+        },
+        {   type :                  "Event",
+            label :                 "Gamification Meeting",
+            discipline :            "Advisory Meeting",
+            date :                  "2010-11-11",
+            relationship :          "Event",
+            "relationship-detail" : "Brainstormed Gamification/Social Gaming Angles for Project",
+            url :                   "knightappsjmeeting111110.txt"
+        },
+        {   type :                  "Event",
+            label :                 "Talent Meeting",
+            discipline :            "Advisory Meeting",
+            date :                  "2010-11-15",
+            relationship :          "Event",
+            "relationship-detail" : "Talent inventory and acquisition discussion. Candidates Identified and recruited",
+            url :                   "knightsitdown111510.txt"
+        },
+        {   type :                  "Team Meeting",
+            label :                 "Post-Hackathon Demo",
+            discipline :            "Team Meeting",
+            date :                  "2011-01-28",
+            relationship :          "Team Meeting",
+            "relationship-detail" : "First Live Demo of Researched Components",
+            url :                   "knightmeeting012811.txt"
+        },
+        {   type :                  "Event",
+            label :                 "Post-Miami Meeting",
+            discipline :            "Advisory Meeting",
+            date :                  "2010-07-21",
+            relationship :          "Event",
+            "relationship-detail" : "Sitdown between PI's and Advisors after President Destler met with Alberto",
+            url :                   "knightmeeting072110.txt"
+        },
+        {   type :                  "Event",
+            label :                 "Mothership Brainstorm",
+            discipline :            "Advisory Meeting",
+            date :                  "2010-09-03",
+            relationship :          "Event",
+            "relationship-detail" : "21 Faculty from 17 Departments present their most mature and enticing projects as candidates for inclusion in the campaign",
+            url :                   "knightmeeting090310.txt"
+        },
+        {   type :                  "Team Meeting",
+            label :                 "Post-Mothership Strategy Meeting",
+            discipline :            "Team Meeting",
+            date :                  "2010-09-22",
+            relationship :          "Event",
+            "relationship-detail" : "It is decided. The OPL Inews/MyNews project is in, and here's what our next steps with Knight will be",
+            url :                   "knightmeeting090310.txt"
+        },
+        {   type :                  "Team Meeting",
+            label :                 "Pre-Winter Break Meeting",
+            discipline :            "Team Meeting",
+            date :                  "2010-12-17",
+            relationship :          "Team Meeting",
+            "relationship-detail" : "The OPL Inews/MyNews project is Demo'd. Evolution of platform is explained. Features are brainstormed. Design Concepts Discussed",
+            url :                   "knightmeeting121710.txt"
+        },
+        {   type :                  "Team Meeting",
+            label :                 "Priorities Meeting",
+            discipline :            "Team Meeting",
+            date :                  "2011-04-01",
+            relationship :          "Team Meeting",
+            "relationship-detail" : "Meetings get shorter. Priorities are discussed. Advisors get out of the way",
+            url :                   "rise040111.txt"
+        },
+        {   type :                  "Design Meeting",
+            label :                 "Mobile App",
+            discipline :            "Design Meeting",
+            date :                  "2011-04-04",
+            relationship :          "Design Meeting",
+            "relationship-detail" : "Mobile app is central theme of meeting. Mockups are made for other HD/Big screen views",
+            url :                   ""
+        },
+        {   type :                  "Team Meeting",
+            label :                 "Mobile Design Critique Meeting",
+            discipline :            "Team Meeting",
+            date :                  "2011-04-08",
+            relationship :          "Team Meeting",
+            "relationship-detail" : "Mobile app mockups are demo'd. Design team gets input for HD/Big Screen Interfaces",
+            url :                   ""
+        },
+        {   type :                  "Team Meeting",
+            label :                 "Final Regular Team Meeting",
+            discipline :            "Team Meeting",
+            date :                  "2011-04-29",
+            relationship :          "Team Meeting",
+            "relationship-detail" : "Shortest yet. T-shirt and Button Designs were printed and shown. Simile Exhibit Widget is Demo'd",
+            url :                   ""
+        },
+        {   type :                  "Test Event",
             label :                 "Test Event #2",
             discipline :            "Test Event",
             date :                  "2011-04-29",
-            relationship :          "Meeting",
-            "relationship-detail" : "First Meeting of Rise above the Crowd",
+            relationship :          "Event",
+            "relationship-detail" : "Second Test Event for our Platform - RIT Spring Fest",
             url :                   "http://www.rit.edu/studentaffairs/cab/springfestival"
         },
     ]
